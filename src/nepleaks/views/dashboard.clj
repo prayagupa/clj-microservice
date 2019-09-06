8e70e64ce69abeb55456ac85665edb050483f82a
Upadhyay <prayag.upd@gmail.com>
2019-09-05 20:27:14 -0700

    initial version of clj service moved from https://github.com/prayagupd/nepleaks-clj

---
+++ src/nepleaks/views/dashboard.clj
@@ -0,0 +1,37 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; dashboard view
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns nepleaks.views.dashboard
+	(:use [hiccup core page]
+	      [clj-http.client :as client]
+              [nepleaks.services.nlpService    :as nlpService]
+              ))
+
+(comment
+	define views)
+
+;;TODO render json here
+
+(defn getJsonResponse []
+  (println "getJsonResponse") ;;FIXME replace it with logger
+  ;; TODO call from nepleaks-engine
+  (println apply str "test")
+  ;; (nlpService/speak "I'm Prayag")
+  ;; (nlpService/tag-page "http://writequit.org")
+  ;; (str "<a>prayagupd</a>") ;;return
+)
+
+(defn dashboard []
+  (html5
+    [:head
+      [:title "nepleaks.org"]
+    ]
+    [:body
+     [:h1 "A leaks pool dashboard"]
+     [:p (getJsonResponse)]
+     [:a {:href "/leak/list"} "Leaks"]
+     [:p ""]
+     [:a {:href "/leaker/list"} "Leakers"]
+     ]))
